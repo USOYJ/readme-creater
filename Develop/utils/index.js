476d209833defccd9dcaf87abf7491c78d0d60ad
Oyin <125850331+USOYJ@users.noreply.github.com>
2023-08-12 15:48:18 -0400

    index

--- Develop/utils/index.js
+++ Develop/utils/index.js
@@ -75,7 +75,7 @@ function writeToFile(fileName, data) {
 function init() {
   inquirer.prompt(questions).then((responses) => {
     console.log("Creating Professional README.md File...");
-    writeToFile("./Users/oyindamolajongbo/Desktop/bootcamp/readme-creater/Develop/utils/Develop/README.md", generateMarkdown({ ...responses }));
+    writeToFile("./Develop/utils/README.md", generateMarkdown({ ...responses }));
   });
 }
 init();
